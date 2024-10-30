30b8d752ebf771384706679b6d94e6ca871f97be
DAW <DAW@10.195.137.170>
2024-10-30 09:09:46 +0100

    ssssa

---
+++ Prueba/src/PruebaGit.java
@@ -0,0 +1,15 @@
+
+public class PruebaGit {
+	 
+	  public static void main(String [] args) {
+		  int num=0;
+		  
+		  do {
+			  System.out.println("el numero es: "+num);
+		  }while(num<10);
+		  
+		  
+	  }
+
+}
+
